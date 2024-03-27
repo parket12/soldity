0283f2a367c35a50d2b678d83107466170699969
parket12 <144909195+parket12@users.noreply.github.com>
2024-03-27 09:08:31 +0300

    Update solidity))).cpp

--- solidity)))/solidity))).cpp
+++ solidity)))/solidity))).cpp
@@ -5,7 +5,7 @@ class BankAccount {
 private:
     int accountNum;
     double accountBalance;
-    double rateInterest;
+    double rateInterest; 
 
 public:
     BankAccount(int num, double balance) : accountNum(num), accountBalance(balance), rateInterest(0.0) {}
@@ -40,11 +40,19 @@ public:
 
     void setInterestRate(double newRate) {
         if (newRate >= 0) {
+            double earnedInterest = getInterest();
+            accountBalance += earnedInterest;
+            cout << "Добавлены проценты в размере: " << earnedInterest << ". Новый баланс: " << accountBalance << endl;
             rateInterest = newRate;
+            cout << "Новая процентная ставка: " << newRate * 100 << "%" << endl;
         }
         else {
             cout << "Процентная ставка не может быть отрицательной!" << endl;
         }
+    }
+
+    double getInterest() const {
+        return accountBalance * rateInterest * (1.0 / 12);
     }
 
     int getAccountNumber() const {
@@ -63,40 +71,50 @@ bool transfer(BankAccount& from, BankAccount& to, double amount) {
 }
 
 void showMenu() {
-    cout << "\n Меню Управления Счетом\n";
+    cout << "\nМеню Управления Счетом\n";
     cout << "1. Показать баланс\n";
     cout << "2. Депозит\n";
     cout << "3. Снять наличные\n";
-    cout << "4. Выход\n";
+    cout << "4. Показать заработанные проценты\n";
+    cout << "5. Изменить процентную ставку\n";
+    cout << "6. Выход\n";
     cout << "Выберите действие: ";
 }
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    BankAccount myAccount(1, 1000); 
+    BankAccount myAccount(1, 1000);
     int choice;
-    double amount;
+    double amount, rate;
 
     while (true) {
         showMenu();
         cin >> choice;
         switch (choice) {
-        case 1: 
+        case 1:
             cout << "Текущий баланс: " << myAccount.getBalance() << endl;
             break;
-        case 2: 
+        case 2:
             cout << "Введите сумму для депозита: ";
             cin >> amount;
             myAccount.deposit(amount);
             break;
-        case 3: 
+        case 3:
             cout << "Введите сумму для снятия: ";
             cin >> amount;
             if (!myAccount.withdraw(amount)) {
                 cout << "Операция не выполнена." << endl;
             }
             break;
-        case 4: 
+        case 4:
+            cout << "Заработанные проценты за месяц: " << myAccount.getInterest() << endl;
+            break;
+        case 5:
+            cout << "Установите новую процентную ставку (в процентах): ";
+            cin >> rate;
+            myAccount.setInterestRate(rate / 100); 
+            break;
+        case 6:
             cout << "Выход из программы..." << endl;
             return 0;
         default:
